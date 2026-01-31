8c063892e6469e16b8c8ef2e4491990d9ac17c0b
AraSlh <ara10@live.de>
2026-01-31 14:16:45 +0100

    Update sketch_task_2_8.ino

--- src/sketch_task_2_8.ino
+++ src/sketch_task_2_8.ino
@@ -1,22 +1,23 @@
-#define VIN_MEAS A3
+#define VIN_MEAS A3  //Pin for reading in the measured input voltage
 
 void setup()
 {
-  pinMode(VIN_MEAS, INPUT);
-  Serial.begin(9600);
+  pinMode(VIN_MEAS, INPUT);  //define the pin as an input
+  Serial.begin(9600);  //open the serial monitor with baud rate 9600
 
-  analogReadResolution(12);
+  analogReadResolution(12);  //set the ADC-reading resolution to 12 bit
 }
 
+//initializing
 int adc_value = 0;
 double adc_voltage = 0, input_voltage = 0;
 
 void loop()
 {
   adc_value = analogRead(VIN_MEAS);
-  adc_voltage = adc_value*(5.0/4095.0);
-  input_voltage = adc_voltage*(4.7+22)/4.7;
-
+  adc_voltage = adc_value*(5.0/4095.0);  //transform the read in adc value into an adc voltage
+  input_voltage = adc_voltage*(4.7+22)/4.7;  //and also transform it to the real input voltage by using the voltage divider formula
+  //Printing all the values
   Serial.print("Input-Voltage: ");
   Serial.print(input_voltage);
 
