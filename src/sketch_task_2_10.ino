d5ad9922f965343a93ce21954caf748e4b943393
AraSlh <ara10@live.de>
2026-01-31 17:33:00 +0100

    Update sketch_task_2_10.ino

--- src/sketch_task_2_10.ino
+++ src/sketch_task_2_10.ino
@@ -1,18 +1,18 @@
 #define USER_LED1 9
 #define BUCK_CNTRL 6
 
-const int PWM_frequency = 50000; //Hz;
+const int PWM_frequency = 50000; //define the PWM frequency to 50 kHz
 
 void setup() {
 
 pinMode(USER_LED1, OUTPUT);
 digitalWrite(USER_LED1, LOW);
-setAnalogWriteFrequency(USER_LED1, PWM_frequency);
+setAnalogWriteFrequency(USER_LED1, PWM_frequency);  //set the frequency to the value above
 }
 
 void loop() {
 
-  analogWrite(USER_LED1, 0.1*getAnalogWriteMaximum());
+  analogWrite(USER_LED1, 0.1*getAnalogWriteMaximum());  //write a PWM-signal with dutycycle of 10%
   delay(1000);
 
 }
